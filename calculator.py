6cb2ba27bde1104e2c9bb45ea8844ac97e546866
sruli <Y0527190457@GMAIL.COM>
2026-02-02 15:08:19 +0200

    Add divide function

--- calculator.py
+++ calculator.py
@@ -7,3 +7,10 @@ def subtract(a, b):
 def multiply(a, b):
     """Multiply two numbers."""
     return a * b
+
+def divide(a, b):
+    """Dividing two numbers"""
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return f"Dividing by 0 is not allowed"
